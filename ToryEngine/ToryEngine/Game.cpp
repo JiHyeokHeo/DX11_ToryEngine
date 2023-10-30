c64c3892d4fb6f5ffcf866267b1ad3417062f7bd
JiHyeok Heo <vndj1212@gmail.com>
2023-10-30 19:38:36 +0900

    WinApi FrameWork
    WinApi 기본 프레임워크

---
+++ ToryEngine/ToryEngine/Game.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "Game.h"
+
+Game::Game()
+{
+}
+
+Game::~Game()
+{
+}
+
+void Game::Init(HWND hwnd)
+{
+	_hwnd = hwnd;
+	_width = GloWinSizeX;
+	_height = GloWinSizeY;
+
+	// TODO
+}
+
+void Game::Update()
+{
+}
+
+void Game::Render()
+{
+}
