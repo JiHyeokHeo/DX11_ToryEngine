49625221229d084631822b5016b0683baa24d4cd
JiHyeok Heo <vndj1212@gmail.com>
2023-11-01 17:56:33 +0900

    장치초기화
    Device & SwapChain
    
    RTV

--- ToryEngine/ToryEngine/Game.h
+++ ToryEngine/ToryEngine/Game.h
@@ -12,12 +12,31 @@ public:
 	void Render();
 
 private:
+	void RenderBegin();
+	void RenderEnd();
+
+private:
+	// 후면 버퍼 제작
+	void CreateDeviceAndSwapChain();
+	// 후면 버퍼에 요청
+	void CreateRenderTargetView();
+	void SetViewPort();
+
+private:
 	HWND _hwnd;
 	uint32 _width = 0;
 	uint32 _height = 0;
 
 private:
-	// DX
+	// Device & SwapChain
+	ComPtr<ID3D11Device> _device;
+	ComPtr<ID3D11DeviceContext> _deviceContext;
+	ComPtr<IDXGISwapChain> _swapChain;
 
+	// RTV (렌더타겟뷰)
+	ComPtr<ID3D11RenderTargetView> _renderTargetView;
+
+	// Misc
+	D3D11_VIEWPORT _viewport = {0};
+	float _clearColor[4] = { 0.5f, 0.5f, 0.5f, 0.5f };
 };
-
